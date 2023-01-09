0cc47df46564f633184b7646a4f9e111006b3b35
fdschepper <333476@student.mboutrecht.nl>
2023-01-09 11:06:06 +0100

    still working on database

--- toets/app/config/config.php
+++ toets/app/config/config.php
@@ -8,7 +8,7 @@ define('DB_NAME', 'mvc-2109b');
 // Het pad naar de app map
 define('APPROOT', dirname(dirname(__FILE__)));
 
-// De URL van de site
+// De URL van deze site
 define('URLROOT', 'http://mvc-oop-pdo-toets-userstory.com');
 
 // Naam van de site
